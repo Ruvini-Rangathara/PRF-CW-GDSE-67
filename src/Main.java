025ba88f761bdbaa4725293952970f843dd5182f
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-07-06 11:46:59 +0530

    Implemented Change Credentials

--- src/Main.java
+++ src/Main.java
@@ -45,15 +45,13 @@ public class Main {
         System.out.println("+|                   WELCOME TO IJSE STOCK MANAGEMENT SYSTEM                |");
         System.out.println("+---------------------------------------------------------------------------+\n");
 
-
         System.out.printf("%-48s%-48s\n", "[1] Change the credentials", "[2] Supplier Manage");
         System.out.printf("%-48s%-48s\n", "[3] Stock Manage", "[4] Logout");
         System.out.println("[5] Exit the system ");
 
-
         int option=0;
         do{
-            System.out.println("\nEnter an option to continue > ");
+            System.out.print("\nEnter an option to continue > ");
             option = input.nextInt();
         }while (!(option>0 && option<6));
         
@@ -69,6 +67,7 @@ public class Main {
     }
 
     private static void exitTheSystem() {
+
     }
 
     private static void logout() {
@@ -84,7 +83,42 @@ public class Main {
     }
 
     private static void changeTheCredentials() {
-        
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("+|                            CREDENTIAL MANAGE                             |");
+        System.out.println("+---------------------------------------------------------------------------+\n");
+
+        System.out.print("Please enter the user name to verify it's you : ");
+        String inputted_username = input.next();
+
+        while(!inputted_username.equals(username)){
+            System.out.println("Invalid user name. Try again!\n");
+            System.out.print("Please enter the user name to verify it's you : ");
+            inputted_username = input.next();
+            System.out.println();
+        }
+
+        System.out.println("Hey "+username);
+
+        System.out.print("Enter your current password : ");
+        String inputted_password = input.next();
+
+        while(!inputted_password.equals(password)){
+            System.out.println("Incorrect password. Try again!\n");
+            System.out.print("Enter your current password : ");
+            inputted_password = input.next();
+            System.out.println();
+        }
+
+        System.out.print("Enter your new password : ");
+        inputted_password = input.next();
+        password=inputted_password;
+
+        System.out.println("Password changed successfully! Do you want to go home page (Y/N) : ");
+        String answer = input.next();
+
+        if(answer.equals("y") || answer.equals("Y")){
+            dashboard();
+        }
     }
 
 
