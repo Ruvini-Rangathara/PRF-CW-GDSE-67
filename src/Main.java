b11351c539f4f95689c2b68ed1a435151f094647
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-07-06 11:26:30 +0530

    Implemented dashboard

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,15 @@ public class Main {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        loadLoginPage();
+//        loadLoginPage();
 
+        dashboard();
     }
 
     private static void loadLoginPage() {
-        System.out.println("+-----------------------------------------------------------------+");
-        System.out.println("+|                           LOGIN PAGE                           |");
-        System.out.println("+-----------------------------------------------------------------+");
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("+|                                LOGIN PAGE                                |");
+        System.out.println("+---------------------------------------------------------------------------+");
 
         System.out.print("User Name : ");
         String inputted_username = input.next();
@@ -34,6 +35,56 @@ public class Main {
             inputted_password = input.next();
         }
 
+        dashboard();
+
+    }
+
+    private static void dashboard() {
+
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("+|                   WELCOME TO IJSE STOCK MANAGEMENT SYSTEM                |");
+        System.out.println("+---------------------------------------------------------------------------+\n");
+
+
+        System.out.printf("%-48s%-48s\n", "[1] Change the credentials", "[2] Supplier Manage");
+        System.out.printf("%-48s%-48s\n", "[3] Stock Manage", "[4] Logout");
+        System.out.println("[5] Exit the system ");
+
+
+        int option=0;
+        do{
+            System.out.println("\nEnter an option to continue > ");
+            option = input.nextInt();
+        }while (!(option>0 && option<6));
+        
+        
+        switch (option){
+            case 1 : changeTheCredentials();
+            case 2 : supplierManage();
+            case 3 : stockManage();
+            case 4 : logout();
+            case 5 : exitTheSystem();
+        }
+
+    }
+
+    private static void exitTheSystem() {
+    }
+
+    private static void logout() {
+        
+    }
+
+    private static void stockManage() {
+        
+    }
+
+    private static void supplierManage() {
+        
+    }
+
+    private static void changeTheCredentials() {
+        
     }
 
 
