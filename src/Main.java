e6b4c28c89b7303b7bca50143640395be66a3bc1
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-07-06 12:51:01 +0530

    Implemented Add supplier

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-
+    static Scanner input = new Scanner(System.in);
     static String username = "Ruvini";
     static String password = "1234";
 
-    static Scanner input = new Scanner(System.in);
+    static String[][] supplier_array = new String[0][2];
+
 
     public static void main(String[] args) {
 //        loadLoginPage();
@@ -40,6 +41,7 @@ public class Main {
     }
 
     private static void dashboard() {
+        clearConsole();
 
         System.out.println("+---------------------------------------------------------------------------+");
         System.out.println("+|                   WELCOME TO IJSE STOCK MANAGEMENT SYSTEM                |");
@@ -67,22 +69,22 @@ public class Main {
     }
 
     private static void exitTheSystem() {
-
+        clearConsole();
+        System.exit(0);
     }
 
     private static void logout() {
-        
+        clearConsole();
+        loadLoginPage();
     }
 
     private static void stockManage() {
-        
-    }
-
-    private static void supplierManage() {
-        
+        clearConsole();
     }
 
     private static void changeTheCredentials() {
+        clearConsole();
+
         System.out.println("+---------------------------------------------------------------------------+");
         System.out.println("+|                            CREDENTIAL MANAGE                             |");
         System.out.println("+---------------------------------------------------------------------------+\n");
@@ -122,4 +124,134 @@ public class Main {
     }
 
 
+    private static void supplierManage() {
+        clearConsole();
+
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("+|                             SUPPLIER MANAGE                              |");
+        System.out.println("+---------------------------------------------------------------------------+\n");
+
+        System.out.printf("%-48s%-48s\n", "[1] Add supplier", "[2] Update supplier");
+        System.out.printf("%-48s%-48s\n", "[3] Delete supplier", "[4] View suppliers");
+        System.out.printf("%-48s%-48s\n", "[5] Search supplier", "[6] Home Page");
+
+        int option=0;
+        do{
+            System.out.print("\nEnter an option to continue > ");
+            option = input.nextInt();
+        }while (!(option>0 && option<6));
+
+
+        switch (option){
+            case 1 : addSupplier();
+            case 2 : updateSupplier();
+            case 3 : deleteSupplier();
+            case 4 : viewSuppliers();
+            case 5 : searchSupplier();
+            case 6 : homepage();
+        }
+    }
+
+    private static void homepage() {
+    }
+
+    private static void searchSupplier() {
+
+    }
+
+    private static void viewSuppliers() {
+
+    }
+
+    private static void deleteSupplier() {
+
+    }
+
+    private static void updateSupplier() {
+
+    }
+
+    private static void addSupplier() {
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("+|                               ADD SUPPLIER                               |");
+        System.out.println("+---------------------------------------------------------------------------+\n");
+
+        String option = "y";
+        while (option.equals("y") || option.equals("Y")){
+            System.out.print("Supplier ID : ");
+            String inputted_supplier_id = input.next();
+            boolean valid = checkSupplierValidity(inputted_supplier_id);
+
+            while(!valid){
+                System.out.print("Already exists. Try another supplier id!");
+                inputted_supplier_id = input.next();
+                valid = checkSupplierValidity(inputted_supplier_id);
+                System.out.println();
+            }
+
+            System.out.print("Supplier Name : ");
+            String inputted_supplier_name = input.next();
+
+            supplier_array = extendSupplierArray(supplier_array);
+            supplier_array[supplier_array.length][0] = inputted_supplier_id;
+            supplier_array[supplier_array.length][1] = inputted_supplier_name;
+
+            System.out.print("Added successfully! Do you want to add another supplier (Y/N) : ");
+            option = input.next();
+        }
+        clearConsole();
+        supplierManage();
+    }
+
+    private static String[][] extendSupplierArray(String[][] supplier_array) {
+        String[][] temp = new String[supplier_array.length+1][2];
+        for (int i = 0; i < supplier_array.length; i++) {
+            temp[i][0] = supplier_array[i][0];
+            temp[i][1] = supplier_array[i][1];
+        }
+        return temp;
+    }
+
+    private static boolean checkSupplierValidity(String inputtedSupplierId) {
+        for (int i = 0; i < supplier_array.length; i++) {
+            if(supplier_array[i][0].equals(inputtedSupplierId)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private final static void clearConsole() {
+        final String os = System.getProperty("os.name");
+        try {
+            if (os.equals("Linux")) {
+                System.out.print("\033\143");
+            } else if (os.equals("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+//handle the exception
+            System.err.println(e.getMessage());
+        }
+    }
 }
